15a3a18cf6321c13d7f5062330965d6bf5fa6ecd
PlasmaDiffusion <Scott50000@gmail.com>
2021-06-25 23:58:06 -0400

    Updated readme + set up mock api tests

--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -1,5 +1,31 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+import "@testing-library/jest-dom/extend-expect";
+
+//Api mocks
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+
+
+//Mock request tests
+const server = setupServer(
+  rest.get("/getFeatures", (req, res, ctx) => {
+    return res(
+      ctx.json([
+        {id: 0,
+        description: "Some feature",
+        votes: 0,
+        userVoted: false,
+        },
+        {id: 1,
+            description: "Some other feature",
+            votes: 1,
+            userVoted: true,
+            }
+      ])
+    );
+  })
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
