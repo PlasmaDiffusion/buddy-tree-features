5dc1c0a04d56fb1a423971336705d2ff4ccbd7c7
PlasmaDiffusion <Scott50000@gmail.com>
2021-06-25 23:30:15 -0400

    Changed first route to allow for arguments

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ migrate = Migrate(app, db)
 
 
 # Render the built front end
-@ app.route("/", defaults={'path': ''})
-def serve(path):
+@ app.route("/<user>", defaults={'path': '', 'user': ''})
+def serve(path, user):
     return send_from_directory(app.static_folder, 'index.html')
 
 
